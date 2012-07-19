a12ea732fc440ce5dd8040e606b3897ca3e8313d
Simon Stearn <simonstearn@gmail.com>
2012-07-19 20:40:28 +0100

    Nothing

--- demo.rb
+++ demo.rb
@@ -49,7 +49,7 @@ before do
   refresh       = session['refresh_token']
   @instance_url = session['instance_url']
   
-  if token && !token.nil?
+  if token && token['errorCode'] == ""
     print "There is a token "
     print token.to_s
     @access_token = ForceToken.from_hash(oauth2_client, { :access_token => token, :refresh_token =>  refresh, :header_format => 'OAuth %s' } )
